e59dcf8cae2dc8d3a76293380916dde41ea51fa2
Arzhuras <arzhuras@gmail.com>
2022-12-01 18:00:32 +0100

    aoc day 1

--- toto.py
+++ toto.py
@@ -1,1 +1,5 @@
-print("hello")
+#AOC 2022 Day 1
+elves = open("toto.txt").read().split("\n\n")
+#print(elves)
+calories = [sum(map(int, elf.split())) for elf in elves]
+print(max(calories), sum(sorted(calories)[-3:]))
