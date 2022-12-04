c5e1fbeedbb5d15b59e4d0f44fc45e3c6563ad28
arzhuras <69085847+arzhuras@users.noreply.github.com>
2022-12-04 08:05:56 +0100

    day 4

---
+++ 2022/4 - Camp Cleanup/AoC_2022_4.py
@@ -0,0 +1,104 @@
+from tools import *
+import time
+
+INPUT_FILE_NAME = "input.txt"
+
+#########################
+### COMMON PROCEDURES ###
+#########################
+
+g_inputLines = []
+
+
+def readInputFile(argFile=INPUT_FILE_NAME):
+    inputLines = []
+    print(f"-> read {argFile}")
+    with open(argFile, "r") as inputFile:
+        for line in inputFile:
+            line = line.rstrip("\n")
+            inputLines.append(line)
+    print(f"  {len(inputLines)} lignes")
+    # print(inputLines)
+    return inputLines
+
+
+#############################
+### INITIALISATION & DATA ###
+#############################
+
+init_script()
+
+g_data = {}
+
+
+def initData():
+    g_data["line"] = []
+
+    for line in g_inputLines:
+        g_data["line"].append(line)
+
+    # print("initData:", g_data)
+
+
+##################
+### PROCEDURES ###
+##################
+
+
+def resolve_part1():
+    print()
+    print(Ansi.red, "### PART 1 ###", Ansi.norm)
+
+    res = 0
+    for line in g_inputLines:
+        a, b = line.split(",")
+        x1, y1 = map(int, a.split("-"))
+        x2, y2 = map(int, b.split("-"))
+        if x1 <= x2 and y1 >= y2:
+            res += 1
+        elif x2 <= x1 and y2 >= y1:
+            res += 1
+
+    return res
+
+
+def resolve_part2():
+    print()
+    print(Ansi.red, "### PART 2 ###", Ansi.norm)
+
+    res = 0
+    for line in g_inputLines:
+        a, b = line.split(",")
+        x1, y1 = map(int, a.split("-"))
+        x2, y2 = map(int, b.split("-"))
+        a = set(range(x1, y1 + 1))
+        b = set(range(x2, y2 + 1))
+        if a & b:
+            res += 1
+
+    return res
+
+
+############
+### MAIN ###
+############
+
+# g_inputLines = readInputFile("sample.txt")
+# g_inputLines = readInputFile("sample2.txt")
+g_inputLines = readInputFile()
+
+initData()
+
+### PART 1 ###
+startTime = time.time()
+res = resolve_part1()
+print()
+print(f"-> part 1 ({time.time() - startTime:.3f}s): {Ansi.blue}{res}{Ansi.norm}")
+
+initData()
+
+### PART 2 ###
+startTime = time.time()
+res = resolve_part2()
+print()
+print(f"-> part 2 ({time.time() - startTime:.3f}s): {Ansi.blue}{res}{Ansi.norm}")
