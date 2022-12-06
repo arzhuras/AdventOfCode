fa81796e194c8a778fa266f8d269d7f775bf1b3e
arzhuras <69085847+arzhuras@users.noreply.github.com>
2022-12-06 06:59:53 +0100

    day 6

---
+++ 2022/6 - Tuning Trouble/AoC_2022_6.py
@@ -0,0 +1,84 @@
+from tools import *
+import time
+
+INPUT_FILE_NAME = "input.txt"
+
+#########################
+### COMMON PROCEDURES ###
+#########################
+
+g_inputLines = []
+
+
+def readInputFile(argFile=INPUT_FILE_NAME):
+    inputLines = []
+    print(f"-> read {argFile}")
+    with open(argFile, "r") as inputFile:
+        for line in inputFile:
+            line = line.rstrip("\n")
+            inputLines.append(line)
+    print(f"  {len(inputLines)} lignes")
+    # print(inputLines)
+    return inputLines
+
+
+#############################
+### INITIALISATION & DATA ###
+#############################
+
+init_script()
+
+g_data = {}
+
+
+def initData():
+    g_data["line"] = []
+
+    for line in g_inputLines:
+        g_data["line"].append(line)
+
+    # print("initData:", g_data)
+
+
+##################
+### PROCEDURES ###
+##################
+
+
+def resolve(marker_size):
+    print()
+    print(Ansi.red, "### PART 1 ###", Ansi.norm)
+    res = 0
+
+    str = g_inputLines[0]
+    for i in range(len(str) - marker_size):
+        marker = set(str[i : i + marker_size])
+        if len(marker) == marker_size:
+            return i + marker_size
+
+    return res
+
+
+############
+### MAIN ###
+############
+
+# g_inputLines = readInputFile("sample.txt")
+# g_inputLines = readInputFile("sample2.txt")
+g_inputLines = readInputFile()
+
+initData()
+
+### PART 1 ###
+startTime = time.time()
+res = resolve(4)
+print()
+print(f"-> part 1 ({time.time() - startTime:.3f}s): {Ansi.blue}{res}{Ansi.norm}")
+
+initData()
+
+### PART 2 ###
+startTime = time.time()
+res = resolve(14)
+print()
+print(f"-> part 2 ({time.time() - startTime:.3f}s): {Ansi.blue}{res}{Ansi.norm}")
