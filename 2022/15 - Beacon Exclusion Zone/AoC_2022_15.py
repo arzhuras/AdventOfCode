4a6f357a4aa55b9725c57af3107e09914dd0c576
arzhuras <69085847+arzhuras@users.noreply.github.com>
2022-12-16 22:00:15 +0100

    day 15

--- 2022/15 - Beacon Exclusion Zone/AoC_2022_15.py
+++ 2022/15 - Beacon Exclusion Zone/AoC_2022_15.py
@@ -1,12 +1,13 @@
 from tools import *
 import time
 from collections import deque
-
-INPUT_FILE_NAME = "input.txt"
+import operator
 
 #########################
 ### COMMON PROCEDURES ###
 #########################
+
+INPUT_FILE_NAME = "input.txt"
 
 
 def readInputFile(argFile=INPUT_FILE_NAME):
@@ -51,8 +52,8 @@ def initData():
         sY = int(fields[2].split()[0])
         bX = int(fields[3].split()[0])
         bY = int(fields[4].split()[0])
-        man = abs(sX - bX) + abs(sY - bY)
-        data.sensors.append(((sX, sY), (bX, bY), man))
+        manhattan = abs(sX - bX) + abs(sY - bY)
+        data.sensors.append(((sX, sY), (bX, bY), manhattan))
 
         data.line.append(line)
 
@@ -68,33 +69,47 @@ def initData():
 def resolve_part1():
     print()
     print(Ansi.red, "### PART 1 ###", Ansi.norm)
-    res = 0
 
     for elt in data.sensors:
         print("##", elt, "##")
         sX, sY = elt[0]
-        bX, bY = elt[1]
-        man = elt[2]
+        manhattan = elt[2]
 
-        for scanLen in range(1, man + 1):
-            # above and below
-            for y in sY - scanLen, sY + scanLen:
-                if y not in data.scanLine:
-                    data.scanLine[y] = []
-                data.scanLine[y].append((sX - y, sX + y, elt[0], man))
-                print("  ", scanLen, y, data.scanLine[y][-1])
+        for offset in range(manhattan + 1):
+            if sY + offset not in data.scanLine:
+                data.scanLine[sY + offset] = []
+            radius = manhattan - offset
+            data.scanLine[sY + offset].append((sX - radius, sX + radius, elt[0], manhattan))
+            # print(sY + offset, data.scanLine[sY + offset])
+            if offset > 0:
+                if sY - offset not in data.scanLine:
+                    data.scanLine[sY - offset] = []
+                data.scanLine[sY - offset].append((sX - radius, sX + radius, elt[0], manhattan))
+                # print(sY - offset, data.scanLine[sY - offset])
 
-        # same line
-        y = sY
-        if y not in data.scanLine:
-            data.scanLine[y] = []
-        data.scanLine[y].append((sX - y, sX + y, elt[0], man))
-        print("  ", scanLen, y, data.scanLine[y][-1])
+    # print(7, data.scanLine[7])
+    # print(10, data.scanLine[10])
+    tmpLst = sorted(data.scanLine[ROW], key=operator.itemgetter(0, 1))
+    print(tmpLst)
+    rangeSize = 0
+    rangeStart, rangeEnd = tmpLst[0][0], tmpLst[0][1]
+    for i in range(1, len(tmpLst)):
+        print(tmpLst[i][0], tmpLst[i][1])
 
-        print(data.scanLine[10])
-        print()
+        if tmpLst[i][0] > rangeEnd:  # nouvelle range
+            rangeSize += rangeEnd - rangeStart
+            rangeStart, rangeEnd = tmpLst[i][0], tmpLst[i][1]
+            print(Ansi.red, "new", Ansi.norm, rangeStart, rangeEnd, rangeSize, tmpLst[i])
+        else:  # overlap
+            if tmpLst[i][1] > rangeEnd:
+                rangeEnd = tmpLst[i][1]
+                print("overlap", rangeStart, rangeEnd, rangeSize)
+            else:
+                print("skip", rangeStart, rangeEnd, rangeSize)
+                pass
+    rangeSize += rangeEnd - rangeStart
 
-    return res
+    return rangeSize
 
 
 def resolve_part2():
@@ -102,16 +117,44 @@ def resolve_part2():
     print(Ansi.red, "### PART 2 ###", Ansi.norm)
     res = 0
 
-    return res
+    for j in range(LIMIT + 1):
+        tmpLst = sorted(data.scanLine[j], key=operator.itemgetter(0, 1))
+        # print(j, tmpLst)
+        rangeStart, rangeEnd = tmpLst[0][0], tmpLst[0][1]
+        for i in range(1, len(tmpLst)):
+            # print(tmpLst[i][0], tmpLst[i][1])
+
+            if tmpLst[i][0] > rangeEnd + 1:  # nouvelle range
+                print(Ansi.red, "BINGO", Ansi.norm, rangeEnd + 1, j)
+                return (rangeEnd + 1) * 400000 + j
+            else:  # overlap
+                if tmpLst[i][1] > rangeEnd:
+                    rangeEnd = tmpLst[i][1]
+                    # print("overlap", rangeStart, rangeEnd)
+                else:
+                    # print("skip", rangeStart, rangeEnd)
+                    pass
+        # print()
+
+    return None
 
 
 ############
 ### MAIN ###
 ############
 
-readInputFile("sample.txt")
-# readInputFile("sample2.txt")
-# readInputFile()
+fileName = "sample2.txt"
+
+ROW = 10
+LIMIT = 20
+fileName = "sample.txt"
+
+# ROW = 2000000
+# LIMIT = 400000
+# fileName = "input.txt"
+
+
+readInputFile(fileName)
 
 initData()
 
@@ -121,9 +164,9 @@ res = resolve_part1()
 print()
 print(f"-> part 1 ({time.time() - startTime:.3f}s): {Ansi.blue}{res}{Ansi.norm}")
 
-exit()
+# exit()
 
-initData()
+# initData()
 
 ### PART 2 ###
 startTime = time.time()
--- 2022/16 - Proboscidea Volcanium/AoC_2022_16.py
+++ 2022/16 - Proboscidea Volcanium/AoC_2022_16.py
@@ -31,6 +31,7 @@ init_script()
 class Data:
     rawInput = []
     line = []
+    valves = {}
 
 
 data = Data()
@@ -40,25 +41,60 @@ def initData():
     data.line = []
 
     for line in data.rawInput:
-        # line = line.replace(",","")
-        # line = line.replace(";","")
-        # line = line.replace("="," ")
+        line = line.replace(",", "")
+        line = line.replace(";", "")
+        line = line.replace("=", " ")
         data.line.append(line)
 
-        # fields = line.split()
+        fields = line.split()
+        data.valves[fields[1]] = [int(fields[5]), fields[10:], False]
 
-    print("initData:", data.line)
+    # print("initData:", data.line)
+    print("valves:", data.valves)
 
 
 ##################
 ### PROCEDURES ###
 ##################
 
+MAX_TIME = 30
+
+
+def checkValve(valve, elapsedtime, tab=""):
+    # print(f"{tab}-> {valve} {elapsedtime} {data.valves[valve][0]} {data.valves[valve][1]} {data.valves[valve][2]}")
+
+    curReleaseOn = 0
+    curReleaseOff = 0
+
+    # skip this valves and go for next
+    if elapsedtime < MAX_TIME and data.valves[valve][2] == False:
+        for next in data.valves[valve][1]:
+            curReleaseOff = checkValve(next, elapsedtime + 1, tab + " ")
+
+    # open this valves if possible and go for next
+    if data.valves[valve][0] > 0 and data.valves[valve][2] == False:
+        data.valves[valve][2] == True
+        curReleaseOn = data.valves[valve][0] * (MAX_TIME - elapsedtime)
+        elapsedtime += 1
+        if elapsedtime < MAX_TIME:
+            for next in data.valves[valve][1]:
+                curReleaseOn = curReleaseOn + checkValve(next, elapsedtime + 1, tab + " ")
+
+    if curReleaseOn > curReleaseOff:
+        # print(f"{tab}  OPEN {curReleaseOn}")
+        return curReleaseOn
+    else:  # keep it closed!
+        # print(f"{tab}  CLOSED {curReleaseOff}")
+        data.valves[valve][2] == False
+        return curReleaseOff
+
 
 def resolve_part1():
     print()
     print(Ansi.red, "### PART 1 ###", Ansi.norm)
     res = 0
+
+    checkValve("AA", 1)
 
     return res
 
