2ed40daa8931da3174e17bbb1d3e61a039072fcb
Arzhuras <arzhuras@gmail.com>
2022-12-19 12:01:06 +0100

    day 19

---
+++ 2022/19 - Not Enough Minerals/AoC_2022_19.py
@@ -0,0 +1,93 @@
+from tools import *
+import time
+from collections import deque
+
+INPUT_FILE_NAME = "input.txt"
+
+#########################
+### COMMON PROCEDURES ###
+#########################
+
+
+def readInputFile(argFile=INPUT_FILE_NAME):
+    data.rawInput = []
+    print(f"-> read {argFile}")
+    with open(argFile, "r") as inputFile:
+        for line in inputFile:
+            line = line.rstrip("\n")
+            data.rawInput.append(line)
+    print(f"  {len(data.rawInput)} lignes")
+    # print(inputLines)
+    return data.rawInput
+
+
+#############################
+### INITIALISATION & DATA ###
+#############################
+
+init_script()
+
+
+class Data:
+    rawInput = []
+    line = []
+
+
+data = Data()
+
+
+def initData():
+    data.line = []
+
+    for line in data.rawInput:
+        data.line.append(line)
+
+    print("initData:", data.line)
+
+
+##################
+### PROCEDURES ###
+##################
+
+
+def resolve_part1():
+    print()
+    print(Ansi.red, "### PART 1 ###", Ansi.norm)
+    res = 0
+
+    return res
+
+
+def resolve_part2():
+    print()
+    print(Ansi.red, "### PART 2 ###", Ansi.norm)
+    res = 0
+
+    return res
+
+
+############
+### MAIN ###
+############
+
+readInputFile("sample.txt")
+# readInputFile("sample2.txt")
+# readInputFile()
+
+initData()
+
+### PART 1 ###
+startTime = time.time()
+res = resolve_part1()
+print()
+print(f"-> part 1 ({time.time() - startTime:.3f}s): {Ansi.blue}{res}{Ansi.norm}")
+
+exit()
+
+initData()
+
+### PART 2 ###
+startTime = time.time()
+res = resolve_part2()
+print()
+print(f"-> part 2 ({time.time() - startTime:.3f}s): {Ansi.blue}{res}{Ansi.norm}")
