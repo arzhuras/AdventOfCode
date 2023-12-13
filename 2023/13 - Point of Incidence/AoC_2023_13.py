9888debae62a99c3eb6816e981c83c13fa38e645
arzhuras <69085847+arzhuras@users.noreply.github.com>
2023-12-13 20:13:00 +0100

    ras

--- 2023/13 - Point of Incidence/AoC_2023_13.py
+++ 2023/13 - Point of Incidence/AoC_2023_13.py
@@ -42,95 +42,126 @@ def initData():
 ### PROCEDURES ###
 ##################
 
-def checkColEquality(grid, col1, col2):
+def checkColDiff(grid, col1, col2):
+    diffCnt = 0
     for row in range(len(grid)):
         if grid[row][col1] != grid[row][col2]:
-            return False
-    return True
+            diffCnt += 1
+    return diffCnt
 
 
-def checkRowEquality(grid, row1, row2):
+def checkRowDiff(grid, row1, row2):
+    diffCnt = 0
     for col in range(len(grid[0])):
         if grid[row1][col] != grid[row2][col]:
-            return False
-    return True
+            diffCnt += 1
+    return diffCnt
 
 
-def checkColSpan(grid, col1, col2):  # col 2 > col 1, col1 et col2 déjà égales
+def checkColSpanDiff(grid, col1, col2):  # col 2 > col 1, col1 et col2 déjà égales
     if (col2 - col1 + 1) % 2 == 1:  # si impair, on skip
-        print(Ansi.yellow, "SKIPPED", col1, col2, Ansi.norm)
-        return False
+        return 2
 
     span = int((col2 - col1 + 1) / 2)
-    # toutes les colonnes dans le span doivent être identiques
+    # toutes les colonnes dans le span doivent être identiques ou avec 1 différence
+    diffs = 0
     for colSpan in range(1, span):
-        if not checkColEquality(grid, col1 + colSpan, col2 - colSpan):
-            return False
-    return True
+        diffs += checkColDiff(grid, col1 + colSpan, col2 - colSpan)
+        if diffs > 1:
+            return 3
+    return diffs
 
 
-def checkRowSpan(grid, row1, row2):  # row 2 > row 1, row 1 et row 2 déjà égales
+def checkRowSpanDiff(grid, row1, row2):  # row 2 > row 1, row 1 et row 2 déjà égales
     if (row2 - row1 + 1) % 2 == 1:  # si impair, on skip
-        print(Ansi.yellow, "SKIPPED", row1, row2, Ansi.norm)
-        return False
+        return 2
 
     span = int((row2 - row1 + 1) / 2)
-    # toutes les colonnes dans le span doivent être identiques
-    for colSpan in range(1, span):
-        if not checkRowEquality(grid, row1 + colSpan, row2 - colSpan):
-            print("checkRowSpan", row1, row2, False)
-            return False
-    return True
+    # toutes les colonnes dans le span doivent être identiques ou avec 1 différence
+    diffs = 0
+    for rowSpan in range(1, span):
+        diffs += checkRowDiff(grid, row1 + rowSpan, row2 - rowSpan)
+        if diffs > 1:
+            return 3
+    return diffs
 
 
-def resolve_part1():
+def resolve():
 
-    patternNotes = 0
-    # hypothèse: un seul axe de symetrie par grid
+    normalPatternNotes = 0
+    smudgePatternNotes = 0
+    # hypothèse: 1 seul axe de symétrie normal et 1 seul smudge par grid
     for gridIdx, grid in enumerate(data.grids):
         print("->", gridIdx)
         # showGrid(grid)
         # check col reflection
         col = 0  # left sided reflection
         for colReflected in range(col+1, len(grid[0]), 2):
-            if checkColEquality(grid, col, colReflected) == True:
-                if checkColSpan(grid, col, colReflected) == True:
-                    span = int((colReflected - col + 1) / 2)
-                    patternNotes += col + span
-                    print("LEFT", col, colReflected, span, patternNotes)
+            diffs = checkColDiff(grid, col, colReflected)
+            if diffs <= 1:
+                diffs += checkColSpanDiff(grid, col, colReflected)
+                span = int((colReflected - col + 1) / 2)
+                match diffs:
+                    case 0:
+                        normalPatternNotes += col + span
+                        print("LEFT", col, colReflected,
+                              span, normalPatternNotes)
+                    case 1:
+                        smudgePatternNotes += col + span
+                        print("LEFT SMUDGE", col,
+                              colReflected, span, smudgePatternNotes)
 
         col = len(grid[0])-1  # right sided reflection
         for colReflected in range(col-1, -1, -2):
-            if checkColEquality(grid, colReflected, col) == True:
-                if checkColSpan(grid, colReflected, col, ) == True:
-                    span = int((col - colReflected + 1) / 2)
-                    patternNotes += colReflected + span
-                    print("RIGHT", colReflected, col, span, patternNotes)
+            diffs = checkColDiff(grid, colReflected, col)
+            if diffs <= 1:
+                diffs += checkColSpanDiff(grid, colReflected, col)
+                span = int((col - colReflected + 1) / 2)
+                match diffs:
+                    case 0:
+                        normalPatternNotes += colReflected + span
+                        print("RIGHT", colReflected, col,
+                              span, normalPatternNotes)
+                    case 1:
+                        smudgePatternNotes += colReflected + span
+                        print("RIGHT SMUDGE", colReflected,
+                              col, span, smudgePatternNotes)
 
         row = 0  # up sided reflection
         for rowReflected in range(row+1, len(grid), 2):
-            if checkRowEquality(grid, row, rowReflected) == True:
-                if checkRowSpan(grid, row, rowReflected) == True:
-                    span = int((rowReflected - row + 1) / 2)
-                    patternNotes += 100 * (row + span)
-                    print("UP", row, rowReflected, span, patternNotes)
+            diffs = checkRowDiff(grid, row, rowReflected)
+            if diffs <= 1:
+                diffs += checkRowSpanDiff(grid, row, rowReflected)
+                span = int((rowReflected - row + 1) / 2)
+                match diffs:
+                    case 0:
+                        normalPatternNotes += 100 * (row + span)
+                        print("UP", row, rowReflected,
+                              span, normalPatternNotes)
+                    case 1:
+                        smudgePatternNotes += 100 * (row + span)
+                        print("UP SMUDGE", row, rowReflected,
+                              span, smudgePatternNotes)
 
         row = len(grid)-1  # down sided reflection
         for rowReflected in range(row-1, -1, -2):
-            if checkRowEquality(grid, rowReflected, row) == True:
-                if checkRowSpan(grid, rowReflected, row) == True:
-                    span = int((row - rowReflected + 1) / 2)
-                    patternNotes += 100 * (rowReflected + span)
-                    print("DOWN", rowReflected, row, span, patternNotes)
+            diffs = checkRowDiff(grid, rowReflected, row)
+            if diffs <= 1:
+                diffs += checkRowSpanDiff(grid, rowReflected, row)
+                span = int((row - rowReflected + 1) / 2)
+                match diffs:
+                    case 0:
+                        normalPatternNotes += 100 * (rowReflected + span)
+                        print("DOWN", rowReflected, row,
+                              span, normalPatternNotes)
+                    case 1:
+                        smudgePatternNotes += 100 * (rowReflected + span)
+                        print("DOWN SMUDGE", rowReflected,
+                              row, span, smudgePatternNotes)
 
-        print()
+        # print()
 
-    return patternNotes
-
-
-def resolve_part2():
-
-    return None
+    return normalPatternNotes, smudgePatternNotes
 
 
 ############
@@ -152,20 +183,17 @@ res = None
 startTime = time.time()
 print()
 print(Ansi.red, "### PART 1 ###", Ansi.norm)
-res = resolve_part1()
+res = resolve()
 print()
 print(
-    f"-> part 1 ({time.time() - startTime:.3f}s): {Ansi.blue}{res}{Ansi.norm}")
+    f"-> part 1 ({time.time() - startTime:.3f}s): {Ansi.blue}{res[0]}{Ansi.norm}")
 
-exit()
-
-initData()
 
 ### PART 2 ###
 startTime = time.time()
 print()
 print(Ansi.red, "### PART 2 ###", Ansi.norm)
-res = resolve_part2()
+# res = resolve()
 print()
 print(
-    f"-> part 2 ({time.time() - startTime:.3f}s): {Ansi.blue}{res}{Ansi.norm}")
+    f"-> part 2 ({time.time() - startTime:.3f}s): {Ansi.blue}{res[1]}{Ansi.norm}")
