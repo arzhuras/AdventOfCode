47aa200c45fdd9f2fe23afb5f2ac8703e3da37e7
Arzhuras <arzhuras@gmail.com>
2024-12-01 17:48:01 +0100

    na

--- 2024/01 - Historian Hysteria/AoC_2024_1.py
+++ 2024/01 - Historian Hysteria/AoC_2024_1.py
@@ -20,8 +20,10 @@ init_script()
 class Data:
     rawInput = None
     fields = None
-
+    
     # grid = None
+    left = None
+    right = None
 
 
 data = Data()
@@ -34,16 +36,22 @@ data = Data()
 
 def initData():
     data.fields = []
+    data.left = []
+    data.right = []
 
     for line in data.rawInput:
         # line = line.replace(".","")
         # line = line.replace(",","")
         # line = line.replace(";","")
         # line = line.replace("="," ")
-        # intFields = list(map(int,line.split()))
+        intFields = list(map(int,line.split()))
+        data.left.append(intFields[0])
+        data.right.append(intFields[1])
         data.fields.append([line.split()])
 
-    print("fields:", data.fields)
+    #print("fields:", data.fields)
+    #print("left:", data.left)
+    #print("rigth:", data.right)
 
     # data.grid = []
     # data.grid = loadMatrix2d(inputFile)[0]
@@ -60,14 +68,45 @@ def initData():
 
 
 def resolve_part1():
-    # grid = data.grid
+    distance = []
+    left = data.left
+    right = data.right
+    while len(left) > 0:
+        smaller_left_idx = 0
+        smaller_left_elt = left[0]
+        for idx, elt in enumerate(left):
+            if elt < smaller_left_elt:
+                smaller_left_idx = idx
+                smaller_left_elt = elt
+        
+        smaller_right_idx = 0
+        smaller_right_elt = right[0]
+        for idx, elt in enumerate(right):
+            if elt < smaller_right_elt:
+                smaller_right_idx = idx
+                smaller_right_elt = elt
 
-    return None
+        distance.append(abs(smaller_right_elt - smaller_left_elt))
+        left.pop(smaller_left_idx)
+        right.pop(smaller_right_idx)
+    #print(distance)
+    return sum(distance)
 
 
 def resolve_part2():
+    similarity = []
+    left = data.left
+    right = data.right
 
-    return None
+    for left_val in left:
+        right_count = 0
+        for right_val in right:
+            if right_val == left_val:
+                right_count += 1
+        similarity.append(left_val * right_count)
+
+    #print(similarity)
+    return sum(similarity)
 
 
 ############
@@ -78,7 +117,7 @@ def resolve_part2():
 inputFile = "sample.txt"
 
 # MAX_ROUND = 1000
-# inputFile = "input.txt"
+inputFile = "input.txt"
 
 data.rawInput = readInputFile(inputFile)
 
@@ -94,7 +133,7 @@ print()
 print(
     f"-> part 1 ({time.time() - startTime:.3f}s): {Ansi.blue}{res}{Ansi.norm}")
 
-exit()
+#exit()
 
 initData()
 res = None
