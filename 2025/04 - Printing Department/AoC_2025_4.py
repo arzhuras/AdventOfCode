cf2125a7ad568b2f0270264f9c599278d3dbbdba
Arzhuras <arzhuras@gmail.com>
2025-12-14 11:01:52 +0100

    Day 4: Printing Department

--- 2025/04 - Printing Department/AoC_2025_4.py
+++ 2025/04 - Printing Department/AoC_2025_4.py
@@ -36,45 +36,19 @@ class Data:
 data = Data()
 
 ###  /modules libraries ###
-# from matrix2d import *
-# MATRIX2D_COLORSET = {"#": Ansi.cyan, "X": Ansi.red}
+from matrix2d import *
+
+MATRIX2D_COLORSET = {"#": Ansi.cyan, "X": Ansi.red, "@": Ansi.green}
 # from matrix3d import *
 # from graph import *
 
 
 def initData():
     data.lineFields = []
-    # data.rules = defaultdict(lambda: set())
-    # data.line = "".join(data.rawInput)
 
-    for line in data.rawInput:
-        # line = line.replace(".","")
-        # line = line.replace(",","")
-        # line = line.replace(";","")
-        # line = line.replace("="," ")
-        # intFields = list(map(int,line.split()))
-        data.lineFields.append([line.split()])
-
-    print("lineFields:", data.lineFields)
-
-    # data.grid = []
-    # data.grid = loadMatrix2d(inputFile)[0]
+    data.grid = []
+    data.grid = loadMatrix2d(inputFile)[0]
     # showGrid(data.grid)
-
-    # data.grids = []
-    # data.grids = loadMatrix2d(inputFile)
-    # showGridLst(data.grid)
-
-    # REGEXP https://pynative.com/python-regex-findall-finditer/
-    # line = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-    # res = re.finditer(r"mul\((?P<a>\d+),(?P<b>\d+)\)|(do\(\))|(don\'t\(\))",data.line)
-    # for match in res:
-    # print(match)
-    # print(match.group())
-    # print(match.group(1))
-    # print(match.group(2))
-    # print(match.group("a"))
-    # print(match.group("b"))
 
 
 ##################
@@ -87,12 +61,42 @@ def resolve_part1():
     return None
 
 
-def resolve_bothpart():
-    # grid = data.gridLst[0]
-    # grid = [["." for x in range(width)] for y in range(height)]
-    # showGrid(grid)
+def accessibleRolls(grid):
+    rollCount = 0
+    newGrid = copy.deepcopy(grid)
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            if grid[y][x] != "@":
+                continue
+            #print(f"({y},{x})={grid[y][x]}")
+            neighbourRollCount = 0
+            for neighbour in OFFSET.AROUND:
+                nx = x + neighbour.x
+                ny = y + neighbour.y
+                if nx >= 0 and nx < len(grid[0]) and ny >= 0 and ny < len(grid):
+                    if grid[ny][nx] == "@":
+                        neighbourRollCount += 1
+            if neighbourRollCount < 4:
+                rollCount += 1
+                newGrid[y][x] = "X"
+    return rollCount, newGrid
 
-    return None, None
+
+def resolve_bothpart():
+    grid = data.grid
+    showGrid(grid, colorset=MATRIX2D_COLORSET, span=2)
+
+    rollCount, rollCount2 = 0, 0
+    tmpRollCount, newGrid = accessibleRolls(grid)
+    showGrid(newGrid, colorset=MATRIX2D_COLORSET, span=2)
+    rollCount = tmpRollCount
+    while tmpRollCount > 0:
+        tmpRollCount, newGrid = accessibleRolls(grid)
+        showGrid(newGrid, colorset=MATRIX2D_COLORSET, span=2)
+        rollCount2 += tmpRollCount
+        grid = newGrid
+
+    return rollCount, rollCount2
 
 
 def resolve_part2():
@@ -108,7 +112,7 @@ def resolve_part2():
 inputFile = "sample.txt"
 
 # MAX_ROUND = 1000
-# inputFile = "input.txt"
+inputFile = "input.txt"
 
 data.rawInput = readInputFile(inputFile)
 # data.gridLst = loadMatrix2d(inputFile)
@@ -128,7 +132,7 @@ print(f"-> part 1 ({endTime - startTime:.6f}s): {Ansi.blue}{res1}{Ansi.norm}")
 
 ### PART 2 ###
 print(Ansi.red, "### PART 2 ###", Ansi.norm)
-initData()
+# initData()
 startTime = time.time()
 # res2 = resolve_part2()
 endTime = time.time()
